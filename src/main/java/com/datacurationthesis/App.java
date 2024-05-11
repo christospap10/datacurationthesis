742fa9262d40a9787aa37f65330a12d120b6b549
christospap10 <xristospapotis@gmail.com>
2024-05-12 01:54:24 +0300

    first commit

---
+++ src/main/java/com/datacurationthesis/App.java
@@ -0,0 +1,14 @@
+package com.datacurationthesis;
+
+/**
+ * Main Class 
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String javaVersion = System.getProperty("java.version");
+        System.out.println( "Java vesion: " + javaVersion);
+    }
+}
