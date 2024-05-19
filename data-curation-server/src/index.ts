7421ed4990f30683d92b542810a6e23b48d85d8c
christospap10 <xristospapotis@gmail.com>
2024-05-19 16:28:47 +0300

    logger

--- data-curation-server/src/index.ts
+++ data-curation-server/src/index.ts
@@ -1,1 +1,2 @@
-console.log("Start server");
+import { logger } from "./logger";
+logger.info("Server started");
---
+++ data-curation-server/src/logger.ts
@@ -0,0 +1,17 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+export { logger };
